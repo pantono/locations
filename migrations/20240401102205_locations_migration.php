1fa857bf081e927d7a31e440beb49663992625ad
Chris Burton <csburton@gmail.com>
2025-09-12 10:13:54 +0100

    Renaming to make IDE completion better

--- migrations/20240401102205_locations_migration.php
+++ migrations/20240401102205_locations_migration.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class Locations extends AbstractMigration
+final class LocationsMigration extends AbstractMigration
 {
     public function up(): void
     {
